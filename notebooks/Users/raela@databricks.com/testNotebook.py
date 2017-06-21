7df5a4423fb6b1675136cf2b7947078a62d79470
raelawang <raela@databricks.com>
2017-06-21 15:12:38 -0700

    version2

--- notebooks/Users/raela@databricks.com/testNotebook.py
+++ notebooks/Users/raela@databricks.com/testNotebook.py
@@ -1,1 +1,17 @@
 # Databricks notebook source
+1+1
+
+# COMMAND ----------
+
+"this is cell 2"
+
+# COMMAND ----------
+
+# MAGIC %md hi
+
+# COMMAND ----------
+
+"cell 4"
+
+# COMMAND ----------
+
